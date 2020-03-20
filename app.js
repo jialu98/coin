18b2eaab8742bc86b8902a3975be1edcced4ed73
Jia Lu <jialu98@gmail.com>
2020-03-19 21:20:30 -0700

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/',(req, res)=>{
-res.send('Hello World!');
+res.send('Hello World!!');
 });
 app.get('/demo',(req, res)=>{
     res.set('X-full-stack', '4life');
